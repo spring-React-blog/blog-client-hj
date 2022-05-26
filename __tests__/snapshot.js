cfd71fa5175cd7764fe3a367fa0bd3af92ceceac
Rockheung <rockheung@gmail.com>
2022-05-26 20:39:41 +0900

    update snapshot for styled-components class system with jest

--- __tests__/snapshot.js
+++ __tests__/snapshot.js
@@ -1,5 +1,6 @@
 import { render } from "@testing-library/react";
 import Home from "~/pages/index";
+import 'jest-styled-components'
 
 it("renders homepage unchanged", () => {
   const { container } = render(<Home />);
